1f4f4980c0ec7421753c30b57e7acef786a80f38
Christian Bilotta <christian.bilotta11@gmail.com>
2024-11-21 21:05:24 -0800

    fixed getUserrepos function

--- backend/GitHubAPI.py
+++ backend/GitHubAPI.py
@@ -1,9 +1,6 @@
-from django.db import models
 import requests
 import random
 import string
-from django.shortcuts import render, redirect
-from django.http import JsonResponse
 from django.conf import settings
 
 class GitHubAPI():
@@ -13,7 +10,9 @@ class GitHubAPI():
     
         #generates a random string of letters and numbers for security
         state = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
-    
+        #stores state for validation for each session
+        request.session['github_state'] = state 
+        
         github_auth_url = (
             #string literal to generate the url for authentication
             f"https://github.com/login/oauth/authorize?client_id={client_id}"
@@ -21,11 +20,31 @@ class GitHubAPI():
         )
         return redirect(github_auth_url)
     
-    def sendUserRepos(request):
-        repos = {
-
+    #url passed in is https://api.github.com/users/USERNAME/repos
+    def getUserRepos(auth_token):
+        #defines headers
+        headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': 'Bearer <YOUR-TOKEN>',
+            'X-GitHub-Api-Version': '2022-11-28',
         }
-        return JsonResponse(repos)
-    
-    def getRepoDetails():
-        return
+
+        #retrieves response from api call
+        response = requests.get('https://api.github.com/user/repos', headers = headers)
+
+        #ensures correct status code
+        if response.status_code == 200:
+            repos = response.json()
+            public_repos = []
+            #fills public_repos with repo data
+            for repo in repos:
+                if not repo["private"]:
+                    public_repos.append({
+                        "name": repo["name"],
+                        "html_url": repo["html_url"],
+                        "description": repo.get("description"),
+                        "language": repo.get("language"),
+                    })
+            return public_repos
+        else:
+            return {f"error: Failed to fetch repositories - Status Code: {response.status_code}"}
