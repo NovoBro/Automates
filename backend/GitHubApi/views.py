64928bf53dfe1a3683902395f6dfb265829aa15e
Cole Joseph Roberts <161370035+cjr03@users.noreply.github.com>
2024-12-06 09:02:22 -0800

    Update views.py

--- backend/GitHubApi/views.py
+++ backend/GitHubApi/views.py
@@ -4,6 +4,28 @@ import string
 import requests
 from django.http import JsonResponse, HttpResponseRedirect
 from django.conf import settings
+from models import Draft
+
+def generate_description_view(request):
+    description = ""
+    if request.method == 'POST':
+        repo_link = request.POST.get('repositoryLink')
+        user_description = request.POST.get('userDescription', "")
+        audience = request.POST.get('audience')
+        tone = request.POST.get('tone')
+        style = request.POST.get('style')
+        hashtags = request.POST.get('hashtags')
+        draft = Draft(
+            repositoryLink=repo_link,
+            userDescription=user_description,
+            postAudience=audience,
+            postTone=tone,
+            postStyle=style,
+            postHashtags=hashtags,
+        )
+        draft.setDescription()
+        description = draft.generatedDescription
+    return render(request, '../Frontend/Text-Gen/scripts/index.html', {'description': description})
 
 #GitHubAPI class
 class GitHubAPI():
