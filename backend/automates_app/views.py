b44f23e700f56875a3ac4e4532a6ed32e735e71f
Cole Joseph Roberts <161370035+cjr03@users.noreply.github.com>
2024-12-06 09:18:04 -0800

    Update views.py

--- backend/automates_app/views.py
+++ backend/automates_app/views.py
@@ -44,6 +44,26 @@ def delete_account(request):
 def accounts(request):
     return render(request, "accounts.html")
 
+def generate_description_view(request):
+    description = ""
+    if request.method == 'POST':
+        repo_link = request.POST.get('repositoryLink')
+        user_description = request.POST.get('userDescription', "")
+        audience = request.POST.get('audience')
+        tone = request.POST.get('tone')
+        style = request.POST.get('style')
+        hashtags = request.POST.get('hashtags')
+        draft = Draft(
+            repositoryLink=repo_link,
+            userDescription=user_description,
+            postAudience=audience,
+            postTone=tone,
+            postStyle=style,
+            postHashtags=hashtags,
+        )
+        draft.setDescription()
+        description = draft.generatedDescription
+    return render(request, '../Frontend/Text-Gen/scripts/index.html', {'description': description})
 
 #GitHubAPI class
 class GitHubAPI():
