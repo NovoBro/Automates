d155fc1146303b31e1dc66a54682bafec10bb380
Cole Joseph Roberts <161370035+cjr03@users.noreply.github.com>
2024-12-06 09:00:48 -0800

    Update urls.py

--- backend/GitHubApi/urls.py
+++ backend/GitHubApi/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import githubAuth, fetchUserRepos, github_callback
+from .views import githubAuth, fetchUserRepos, github_callback, generate_description_view
 
 urlpatterns = [
     path('auth/', githubAuth, name='github_authenticate'),
     path('repos/', fetchUserRepos, name='fetch_user_repos'),
     path('callback/', github_callback, name='github_callback'),
+    path('generate-description/', generate_description_view, name='generate_description')
 ]
